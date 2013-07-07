62859e0e723c8e1faccb5b55a11bde15e0c0bb41
Karan Chitnis <karanchitnis92@gmail.com>
2013-07-07 05:44:17 -0700

    editable posts

--- app/assets/javascripts/wall.js
+++ app/assets/javascripts/wall.js
@@ -25,11 +25,20 @@
     //$(".post").append('<img id="delete" src="assets/delete.png" />').css("opacity", "1" );
     var postIDstr = postID.toString();  
     var delhtml = "<img id='delete" + postIDstr + "' class='delete' src='assets/delete.png' />"
-  
+  /*  
+    var boldhtml = "<button type='button' id='bold" + postIDstr + "' class='bold'>B</button>";
+    var italhtml = "<button type='button' id='italic" + postIDstr + "' class='italic'>I</button>";
+    var underhtml = "<button type='button' id='underline" + postIDstr + "' class='underline'>U</button>";
+  */  
     $("#post" + postIDstr).append($('#edit' + postIDstr));
     $("#post" + postIDstr).append($('#content' + postIDstr));
     $("#post" + postIDstr).append($('#accessor' + postIDstr));
-
+    $("#edit" + postIDstr).append(delhtml)
+  /*
+    $("#edit" + postIDstr).append(boldhtml)
+    $("#edit" + postIDstr).append(italhtml)
+    $("#edit" + postIDstr).append(underhtml)
+  */
     $(".post").draggable().click(function(){
       if ($(this).is('.ui-draggable-dragging')) {
         return;
@@ -53,7 +62,49 @@
       $("#post" + deleteid).remove();
     });
 
+/*
+    $(".bold").click(function(){
+      //var contentid = $(this).attr('id');
+      alert(contentid);
+      //contentid = contentid.substring(4);
+      //console.log(contentid);
+      //$(".content").attr('contenteditable', true);
+      $(".content").attr('contenteditable', true);
+      document.execCommand('bold',false,null);
+    });
+    });
+
+    $(".italic").click(function(){
+      var contentid = $(this).attr('id');
+      contentid = contentid.substring(6);
+      $("#content" + contentid).attr('contenteditable', true);
+      document.execCommand('italic',false,null);
+    });
+
+    $(".underline").click(function(){
+      var contentid = $(this).attr('id');
+      contentid = contentid.substring(9);
+      $("#content" + contentid).attr('contenteditable', true);
+      document.execCommand('underline',false,null);
+    });
+*/
+
     postattr.style.left = e.clientX + 'px';
     postattr.style.top = e.clientY + 'px';
     postID += 1;
   }, false);
+
+$(document).ready(function(){
+  $(".bold").click(function(){
+    $(".content").attr('contenteditable', true);
+    document.execCommand('bold',false,null);
+  });
+  $(".italic").click(function(){
+    $(".content").attr('contenteditable', true);
+    document.execCommand('italic',false,null);
+  });
+  $(".underline").click(function(){
+    $(".content").attr('contenteditable', true);
+    document.execCommand('underline',false,null);
+  });
+});
